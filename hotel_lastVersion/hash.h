a079000cf755c6b77be089a15dba3f797c19b030
Svetlanaaa <matveeva.si@mail.ru>
2016-12-12 18:32:20 +0300

    хэш-таблица гостей отеля

--- hotel_lastVersion/hash.h
+++ hotel_lastVersion/hash.h
@@ -1,66 +1,20 @@
 #pragma once
 #include "guest.h"
+#include <fstream>
 
-const int MAX = 180;
-typedef struct{
-	int index;   /* ëîãè÷åñêèé èíäåêñ */
-	Guest* g;     /* ñîáñòâåííî çíà÷åíèå ýëåìåíòà äàííûõ */
-	hashType *next; /* óêàçàòåëü íà ñëåäóþùèé ýëåìåíò ñ òàêèì æå
-						õýø-àäðåñîì */
-} hashType;
+const int MAX = 97; 
+struct hashType {
+	int index;   
+	Guest* g;    
+	struct hashType *next; 
+};
 
 class Hash {
-	hashType hashMas[MAX];
+	struct hashType hashMas[MAX];
 public:
-	Hash() {
-		for (int i = 0; i < MAX; i++) {
-			hashMas[i].index = -1;
-			hashMas[i].next = NULL;  
-			hashMas[i].g = NULL;
-		}
-	}
-
-	int hashF(Guest *g)
-	{
-		int res;
-		res = (g->getPassport() % pow(10,6)) / MAX;
-		res = res + (g->getPassport() / pow(10, 6)) / MAX;
-		return res;
-	}
-
-	void add(Guest *g)
-	{
-		int h;
-		hashType *p;
-		h = hashF(g);
-
-		if (hashMas[h].index == -1) {
-			hashMas[h].index = h;
-			hashMas[h].g = g;
-		}
-		else
-		{
-			hashType *temp = new hashType; 
-			temp->next = hashMas[h].next; 
-			temp->index = h;
-			temp->g = g;
-			hashMas[h].next = temp;
-		}
-	}
-
-	Guest* find(Guest *g)
-	{
-		int h;
-		hashType *p;
-		h = hashF(g);
-		if (hashMas[h].g == g) return(hashMas[h].g);
-		else { 
-			p = hashMas[h].next;
-			while (p) {
-				if (p->g == g) return p->g;
-				p = p->next;
-			}
-			return NULL;
-		}
-	}
-}
+	struct hashType* getHash() { return hashMas; };
+	Hash();
+	int hashF(Guest*);
+	void add(Guest*);
+	Guest* find(Guest*);
+};
